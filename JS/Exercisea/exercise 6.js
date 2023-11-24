4f41636174a12eca16ab19efbda93ca6cbc9b73e
DaiZu2 <151722827+DaiZu2@users.noreply.github.com>
2023-11-24 14:53:16 +0100

    Delete JS/Exercisea directory

--- JS/Exercisea/exercise 6.js
+++
@@ -1,7 +0,0 @@
-let users = [
-    { name: "Bozydar", age: 301 },
-    { name: "Daniel", age: 4467923 },
-    { name: "Janusz Duponogi", age: 2 }
-];
-let names = users.map(user => user.name);
-console.log(names); ["Bozydar", "Daniel", "Janusz Duponogi"]
